92e46ed82b5f1e037e5dc9fffe9c23ae163fd944
skkim125 <kthanks125@gmail.com>
2024-08-14 23:28:32 +0900

    Initial Commit

---
+++ Musictory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Musictory
+//
+//  Created by 김상규 on 8/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
